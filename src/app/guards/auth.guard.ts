8b61e4e2a57df98e3c5d9f78eaa27a01eb0af95a
Vincent O <vincento@live.se>
2022-08-30 14:51:26 +0200

    pages blocked and new guard created to protect routes only reachable be logged in users

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { OrganicRouteService } from '../services/organic-route.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardGuard implements CanActivate {
+export class AuthGuard implements CanActivate {
+  constructor(private organicroute: OrganicRouteService) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const role = sessionStorage.getItem("role");
     const loginstatus = sessionStorage.getItem("loggedin")
-    if (role === "ADMIN" && loginstatus === "true") {
-      return true;
+    if (loginstatus === "true") {
+      return true
+    } else {
+      console.log("you need to login")
+      this.organicroute.openLogin();
+      return false;
     }
-    return false;
   }
+
 }
---
+++ src/app/services/organic-route.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganicRouteService {
+
+  constructor(private router: Router, private location: Location) { }
+  openLogin() {
+    this.router.navigate(['login']);
+  }
+  openHome() {
+    this.router.navigate(['home']);
+  }
+
+  navigateBack() {
+    this.location.back();
+  }
+  openAdmin() {
+    this.router.navigate(['admin-dashboard']);
+  }
+
+  openUserDasdhboard() {
+    this.router.navigate(['mydebt'])
+  }
+}
