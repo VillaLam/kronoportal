a4191c3eae2c052e6d681d6746aab8ff3777d0da
Villacorta <daniel.villacorta@cgi.com>
2022-08-31 11:24:00 +0200

    add debt to user

--- src/app/components/debt-registration/debt-registration.component.ts
+++ src/app/components/debt-registration/debt-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserServiceService } from 'src/app/services/user-service.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class DebtRegistrationComponent implements OnInit {
 
   public findBySocialSecurityNumber: FormGroup;
 
-  constructor(private fb: FormBuilder, private authservice: UserServiceService) {
+  constructor(private fb: FormBuilder, private authservice: UserServiceService, private router: Router) {
     this.findBySocialSecurityNumber = this.fb.group({
       socialSecurityNumber: new FormControl(''),
     })
@@ -25,7 +26,10 @@ export class DebtRegistrationComponent implements OnInit {
     let SSN = this.findBySocialSecurityNumber.controls["socialSecurityNumber"].value;
     this.authservice.getUserBySocialSecurityNumber(SSN).subscribe(
       (res) => {
+        sessionStorage.setItem("tempUserId", res.userId);
         console.log("User has been found! : ", res)
+
+        this.router.navigate(['newdebt'])
       }
     )
   }
